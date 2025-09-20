096fc1a536b322327171c0c51f7561c2f03d80a6
paulwconnor-ai <paulwconnor.ai@outlook.com>
2025-09-20 15:47:33 +0100

    Restored desired orth projection, after downgrade of bevy

--- src/bundles/camera.rs
+++ src/bundles/camera.rs
@@ -1,5 +1,5 @@
 use bevy::prelude::*;
-use bevy::render::camera::{Camera, OrthographicProjection, Projection, ScalingMode};
+use bevy::render::camera::ScalingMode;
 
 use crate::constants::{LOGICAL_H, LOGICAL_W};
 
@@ -9,25 +9,16 @@ pub fn camera_2d_bundle() -> Camera2dBundle {
             clear_color: ClearColorConfig::Custom(Color::rgb(1.0, 1.0, 1.0)),
             ..default()
         },
-        ..default()
-    }
-}
-
-/*pub fn camera_2d_bundle() -> impl Bundle {
-    (
-        Camera2d,
-        Camera {
-            clear_color: ClearColorConfig::Custom(Color::rgb(1.0, 1.0, 1.0)),
-            ..default()
-        },
-        Projection::Orthographic(OrthographicProjection {
+        projection: OrthographicProjection {
             scaling_mode: ScalingMode::Fixed {
                 width: LOGICAL_W,
                 height: LOGICAL_H,
             },
-            ..OrthographicProjection::default()
-        }),
-        Transform::default(),
-        GlobalTransform::default(),
-    )
-}*/
+            near: -1000.0,
+            far: 1000.0,
+            ..default()
+        }
+        .into(),
+        ..default()
+    }
+}
