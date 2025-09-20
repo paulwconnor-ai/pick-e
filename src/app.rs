19341bd6f18cf028d7a00a4de53ad8a5c29bbcdc
paulwconnor-ai <paulwconnor.ai@outlook.com>
2025-09-20 15:52:36 +0100

    Some comments and tidy-up

--- src/app.rs
+++ src/app.rs
@@ -7,12 +7,20 @@ use crate::systems::startup::setup;
 pub fn build_app() -> App {
     let mut app = App::new();
 
+    // add default plugins:
     app.add_plugins(DefaultPlugins.set(AssetPlugin {
         file_path: "assets".into(),
         ..Default::default()
     }));
 
+    // Rapier physics global setup:
+    app.insert_resource(RapierConfiguration {
+        gravity: Vec2::ZERO, // disable gravity in rapier physics
+        ..Default::default()
+    });
     app.add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0));
+
+    // add our startup system:
     app.add_systems(Startup, setup);
 
     app
--- src/systems/startup.rs
+++ src/systems/startup.rs
@@ -3,12 +3,10 @@ use crate::bundles::hero::hero_bundle;
 use bevy::log::info;
 use bevy::prelude::*;
 use std::env;
-use std::path::Path;
 
 pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    info!("Running from {:?}", env::current_dir().unwrap());
+    info!("Running Pick.e from {:?}", env::current_dir().unwrap());
 
-    info!("📷 Spawning camera...");
     commands.spawn(camera_2d_bundle());
     commands.spawn(hero_bundle(&asset_server));
 }
