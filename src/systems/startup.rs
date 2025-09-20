88b98b41a3bf25282042834e991c256132ae319a
paulwconnor-ai <paulwconnor.ai@outlook.com>
2025-09-20 22:28:42 +0100

    Got wasm version compiling again

--- src/systems/startup.rs
+++ src/systems/startup.rs
@@ -7,8 +7,6 @@ use bevy::prelude::*;
 use std::env;
 
 pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    info!("Running Pick.e from {:?}", env::current_dir().unwrap());
-
     commands.spawn(camera_2d_bundle());
     commands.spawn(hero_bundle(&asset_server));
 
